d8410f1d2fe912ec7789163949faa210bfdc37f3
amankumar-dev <rjvkha@gmail.com>
2026-03-16 20:37:55 +0530

    Initial Commit

--- db.py
+++ db.py
@@ -5,14 +5,28 @@ from dotenv import load_dotenv
 # Load .env file
 load_dotenv()
 
-# Create connection with postgres
-conn=psycopg2.connect(
-    dbname=os.getenv("DB_NAME"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    host=os.getenv("DB_HOST"),
-    port=os.getenv("DB_PORT")
-)
-
-# Create cursor
+# Creating Connection with postgresql
+def get_connection():
+    try:
+        conn=psycopg2.connect(
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT")
+        )
+        return conn
+    
+    except Exception as e:
+        print('Database connection failed !! ',e)
+        return None
+    
+# Establishing the connection
+conn=get_connection()
+if conn:
+    print('Connection established !!')
+else:
+    print('Connection Failed !!')
+    
+# Creating Cursor
 cursor=conn.cursor()
