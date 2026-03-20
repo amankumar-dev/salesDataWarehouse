130d67d7c70d76d2a94f1f9d5360c23a25c60b16
amankumar-dev <rjvkha@gmail.com>
2026-03-20 18:27:45 +0530

    'update'

--- etl.py
+++ etl.py
@@ -12,3 +12,11 @@ salesDf=pd.read_csv(r'D:\Aman\aman.code\PostgreSQL\Sales_Data_Warehouse_Project\
 # Dataframe of stores data
 storeDf=pd.read_csv(r'D:\Aman\aman.code\PostgreSQL\Sales_Data_Warehouse_Project\data\stores.csv')
 
+custDf=custDf[['customer_name','email','city']]
+
+prodDf=prodDf[['product_name','category','cost_price','unit_price']]
+
+storeDf=storeDf[['store_name','region']]
+
+salesDf['order_date']=pd.to_datetime(salesDf['order_date'])             # Converting normal column into datetime column
+salesDf=salesDf.drop(columns=['order_id'])
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+    query='''INSERT INTO order_table(custId,prodId,storeId,quantity,unitprice,costprice,revenue,costamount,profit,order_date) VALUES %s;'''
